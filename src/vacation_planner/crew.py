cfa91db31ac5d4f36f9bf58ad7a595846ed649d7
Sadegh Etemad <Sadegh.Etemad@uk-cpi.com>
2026-04-01 12:02:09 +0100

    feat: initial commit

---
+++ src/vacation_planner/crew.py
@@ -0,0 +1,78 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai_tools import SerperDevTool
+from dotenv import load_dotenv
+import os
+from crewai import LLM
+
+load_dotenv()
+
+# Initialize the tool for internet searching capabilities
+serper_tool = SerperDevTool(api_key = os.getenv("SERPER_API_KEY"))
+
+llm = LLM(model = os.getenv("MODEL"), aws_region_name = "AWS_DEFAULT_REGION")
+
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+@CrewBase
+class VacationPlanner():
+    """VacationPlanner crew"""
+
+    agents: list[BaseAgent]
+    tasks: list[Task]
+
+    # Learn more about YAML configuration files here:
+    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+    
+    # If you would like to add tools to your agents, you can learn more about it here:
+    # https://docs.crewai.com/concepts/agents#agent-tools
+    @agent
+    def vacation_researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['vacation_researcher'], # type: ignore[index]
+            verbose=True,
+            tools=[serper_tool],
+            llm = llm
+        )
+
+    @agent
+    def itinerary_planner(self) -> Agent:
+        return Agent(
+            config=self.agents_config['itinerary_planner'], # type: ignore[index]
+            verbose=True,
+            llm = llm
+        )
+
+    # To learn more about structured task outputs,
+    # task dependencies, and task callbacks, check out the documentation:
+    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task'], # type: ignore[index]
+        )
+
+    @task
+    def reporting_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['reporting_task'], # type: ignore[index]
+            output_file='report.md'
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the VacationPlanner crew"""
+        # To learn how to add knowledge sources to your crew, check out the documentation:
+        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
+        return Crew(
+            agents=self.agents, # Automatically created by the @agent decorator
+            tasks=self.tasks, # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
