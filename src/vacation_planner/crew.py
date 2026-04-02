66f0b7ccf8b0b840895ad86375f1c69ad8db9ce4
Sadegh Etemad <Sadegh.Etemad@uk-cpi.com>
2026-04-02 18:34:22 +0100

    feat: add memory to crew

--- src/vacation_planner/crew.py
+++ src/vacation_planner/crew.py
@@ -8,14 +8,25 @@ from crewai import LLM
 
 from bedrock_agentcore.runtime import BedrockAgentCoreApp
 
+# use for memory
+import uuid
+from datetime import datetime, UTC
+import boto3
+
+
+memory_client = boto3.client(service_name = 'bedrock-agentcore', region_name = 'us-west-2')
+
+
 app = BedrockAgentCoreApp()
 
-load_dotenv()
+# load_dotenv()
+# MODEL = os.environ.get("MODEL")
+# AWS_REGION = os.environ.get("AWS_DEFAULT_REGION")
+# SERPER_API_KEY = os.environ.get("SERPER_API_KEY")
 
 # Initialize the tool for internet searching capabilities
-serper_tool = SerperDevTool(api_key = os.getenv("SERPER_API_KEY"))
-
-llm = LLM(model = os.getenv("MODEL"), aws_region_name = "AWS_DEFAULT_REGION")
+serper_tool = SerperDevTool(api_key = "3597ef4a837f1618e2268ed7b03cb4e2621c5360")
+llm = LLM(model = "bedrock/us.amazon.nova-pro-v1:0", aws_region_name = "us-west-2")
 
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
@@ -92,18 +103,75 @@ def agent_invocation(payload, context):
         user_input = payload.get('topic', 'London, UK')
         print(f"Processing Vacation Destination: {user_input}")
 
+        # retrieve the past memory
+        session_id = getattr(context, "sessionId", "default_session")
+
+        previous_events = memory_client.list_events(
+            memoryId = 'vacation_planner-Go9RLHHjGB',
+            actorId = 'user',
+            sessionId = session_id,
+            maxResults = 3
+        )
+
         research_vacation_planner = VacationPlanner()
         crew = research_vacation_planner.crew()
-        result = crew.kickoff(inputs={'topic': user_input})
+
+        # result = crew.kickoff(inputs={'topic': user_input})
+
+        # Send input to Agent with previous memory
+        events = previous_events.get('events', [])
+        formatted_conversations = []
+        for event in events:
+            formatted_event = {}
+            for key, value in event.items():
+                if isinstance(value, datetime):
+                    formatted_event[key] = value.isoformat()
+                else:
+                    formatted_event[key] = value
+            formatted_conversations.append(formatted_event)
+        
+        #4 Starts the sequential agent workflow with memory
+        result = crew.kickoff(inputs={'topic': user_input, 'previous_conversations': formatted_conversations})
+        
+        #5 Memory storage - Save current interaction
+        memory_client.create_event(
+            memoryId='vacation_planner-Go9RLHHjGB',
+            actorId='user',
+            sessionId=session_id,
+            eventTimestamp=datetime.now(UTC),
+            payload=[
+                {
+                    "conversational": {
+                        "content": {"text": user_input},
+                        "role": "USER"
+                    }
+                },
+                {
+                    "conversational": {
+                        "content": {"text": result.raw},
+                        "role": "ASSISTANT"
+                    }
+                }
+            ],
+            clientToken=str(uuid.uuid4())
+        )
+
 
         print(f'Result: {result.raw}')
         print(f"Context: {context}")
 
+        # Safely access json_dict if it exists
+        if hasattr(result, 'json_dict'):
+            print("Result JSON:\n*******\n", result.json_dict)
+        
+        return {"result": result.raw}
+
     except Exception as err:
         print(f"Error: {err}")
+        return {"error": f"An error occurred: {str(err)}"}
 
 
 if __name__ == "__main__":
 
     # Run agentcore server
-    app.run()
+    app.run(port=8080)
